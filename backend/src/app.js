84d7fbd2eac79ad35c74f78c2b4f4d75ad598659
YasserAbdalla <yasserabdalla149@gmail.com>
2024-09-18 21:29:55 +0300

    initialize the db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://iti_db_project:itX836IrVSSfOzR1@cluster0.swbigys.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("DB connected successfuy!");
+  })
+  .catch((error) => {
+    console.log("The error info: ", error.message);
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Hi demo" });
