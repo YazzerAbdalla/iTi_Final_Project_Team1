4263568473a2fe12fa08ec6d69a3bc4c4345b2cb
YazzerAbdalla <yasserabdalla149@gmail.com>
2024-09-30 22:31:43 +0300

    Handle the add item button error while not logged in

--- frontend/src/app/components/product-details/product-details.component.ts
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../types/Product';
 import { ProductService } from '../../services/product.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { AuthService } from '../../services/auth.service';
 
 interface AddItemToCartForUser {
   id: string;
   quantity: number;
 }
+
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css',
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
   product!: Product;
   loading = true;
+  isUserLoggedIn = false; // Track whether the user is logged in
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private cartService: CartService
+    private router: Router,
+    private cartService: CartService,
+    private authService: AuthService // Inject AuthService
   ) {}
 
   ngOnInit(): void {
@@ -33,9 +37,19 @@ export class ProductDetailsComponent implements OnInit {
         this.loading = false;
       });
     }
+
+    // Subscribe to email to check if the user is logged in
+    this.authService.getUserEmail().subscribe((email) => {
+      this.isUserLoggedIn = !!email; // Set to true if email exists
+    });
   }
 
-  addItemToCartForUser({ id, quantity }: AddItemToCartForUser) {
-    this.cartService.addToCart({ id, quantity });
+  addItemToCartForUser({ id, quantity }: AddItemToCartForUser): void {
+    if (!this.isUserLoggedIn) {
+      this.router.navigate(['/login']); // Redirect to login if not logged in
+    } else {
+      // Add item to cart if the user is logged in
+      this.cartService.addToCart({ id, quantity });
+    }
   }
 }
